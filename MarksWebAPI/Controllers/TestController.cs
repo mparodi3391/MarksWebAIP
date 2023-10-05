4923c77053d309ae46807d94954b5cef4877c214
martin.parodi <martin.parodi@fing.edu.uy>
2023-10-05 07:56:52 -0300

    Agregar archivos de proyecto.

---
+++ MarksWebAPI/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using MarksWebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MarksWebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet(Name = "GetTesting")]
+        public IEnumerable<User> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new User
+            {
+                Id = index,
+                Name = "Testing",
+                Email = "admin@testing.com"
+            })
+            .ToArray();
+        }
+    }
+}
